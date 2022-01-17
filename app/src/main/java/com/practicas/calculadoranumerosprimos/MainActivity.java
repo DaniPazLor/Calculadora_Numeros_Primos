40208cb8f88a915fa80d44d4a07f5244f9826233
DaniPazLor <85082509+DaniPazLor@users.noreply.github.com>
2022-01-17 16:34:33 +0100

    Tarea completa v.1

--- app/src/main/java/com/practicas/calculadoranumerosprimos/MainActivity.java
+++ app/src/main/java/com/practicas/calculadoranumerosprimos/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int resultado=10;
+    public int resultado;
     public Editable posicion;
 
     @Override
@@ -19,16 +19,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void resultado(View view){
+
+        EditText editTextPosicion = findViewById(R.id.editTxtPosicion);
+        posicion=editTextPosicion.getText();
+
+        calculoNumerosPrimos(Integer.parseInt(posicion.toString()));
+
+    }
+
+    public void calculoNumerosPrimos(int posicion) {
+        int contador;
+        int numero = 3;
+        boolean primo;
+        int posNumPrimo = 2;
+
+        if (posicion == 1) {
+            mostrarResultado(posicion);
+        }else if(posicion == 2){
+            mostrarResultado(posicion);
+        }else{
+            do{
+                contador=2;
+                primo=true;
+                while ((primo) && (contador!=numero)){
+                    if (numero % contador == 0){
+                        primo = false;
+                    }
+                    contador++;
+                }
+                if (primo){
+                    posNumPrimo++;
+                }
+                if (posNumPrimo==posicion){
+                    mostrarResultado(numero);
+                }
+                numero++;
+            }while (posNumPrimo < posicion);
+
+        }
 
 
     }
 
-    public void resultado(View view){
+    public void mostrarResultado(int resultado){
         TextView txtResultado = (TextView) findViewById(R.id.txtResultado);
-        EditText editTextPosocion = findViewById(R.id.editTxtPosicion);
-        posicion=editTextPosocion.getText();
-
-        txtResultado.setText(posicion);
+        txtResultado.setText(String.valueOf(resultado));
         txtResultado.setVisibility(View.VISIBLE);
     }
 }
